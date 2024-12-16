47bea4ecea76272148dd5213626afa12b7e67754
Ilyas Founas <ifounas@z2r8p3.42lyon.fr>
2024-12-16 15:15:24 +0100

    new version with the library and without useless includes

--- get_next_line.h
+++ get_next_line.h
@@ -6,19 +6,17 @@
 /*   By: ifounas <ifounas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/27 15:43:12 by ifounas           #+#    #+#             */
-/*   Updated: 2024/12/11 17:35:39 by ifounas          ###   ########.fr       */
+/*   Updated: 2024/12/16 15:14:02 by ifounas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
-
 # define GET_NEXT_LINE_H
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1
+# endif
 
-# include <fcntl.h>
-# include <limits.h>
-# include <stdarg.h>
 # include <stdint.h>
-# include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
 
@@ -27,7 +25,6 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 size_t	ft_strlen(char const *s, int c);
 char	*ft_strjoin(char *s1, char const *s2);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
-//char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_substr(char *s, unsigned int start, size_t len);
 
 #endif
